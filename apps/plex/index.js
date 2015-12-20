646a68d5a6944644e62964056428214da11924ec
Kevin van Dijk <kevin@edgybits.com>
2015-12-20 22:40:46 +0100

    Add test intent to start a tv show or movie

--- apps/plex/index.js
+++ apps/plex/index.js
@@ -1,4 +1,16 @@
 var alexa = require('alexa-app');
+var Promise = require('bluebird');
+var fetch = require('node-fetch');
+var dotenv = require('dotenv');
+
+fetch.Promise = Promise;
+
+dotenv.load();
+
+function apiUrl(path) {
+  return process.env.HOMER_ADDRESS + '/api/plex/' + path;
+};
+
 var app = new alexa.app('homer-plex');
 
 app.pre = function(req, res, type) {
@@ -10,8 +22,41 @@ app.pre = function(req, res, type) {
 };
 
 app.launch(function(req, res) {
-  res.say('Available And stuff');
+  res.say('awesome it works' + process.env.HOMER_ADDRESS);
   res.shouldEndSession(false);
+});
+
+app.intent('startShowOrMovie', {
+  slots: {
+    'NAME': 'LITERAL'
+  },
+  utterances: [
+    'start {homeland|NAME}',
+    'start playing {homeland|NAME}',
+    'play {homeland|NAME}'
+  ]
+}, function(req, res) {
+  var params = {
+    name: req.slot('NAME')
+  };
+
+  fetch(apiUrl('start'), {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify(params)
+  }).then(function(response) {
+    return response.json();
+  }).then(function(response) {
+    res.say('Playing');
+    res.send();
+  }).catch(function(error) {
+    res.say('Something went wrong');
+    res.send();
+  });
+
+  return false;
 });
 
 // Export both as lambda handler and as alexa-app for alexa-app-server debugging
